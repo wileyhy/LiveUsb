7363f3a7ecf63a8ce7dd511297a6174428fefabb
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 15:05:00 -0700

    ./test/test-err-trap-v-pipefail_2.sh, debugging
    Add in a stand-alone call to `gh auth status` to see its return code

--- test/test-err-trap-v-pipefail_2.sh
+++ test/test-err-trap-v-pipefail_2.sh
@@ -10,6 +10,9 @@ set -o pipefail
 trap_exit(){ local - exit_ec="$?"; : 'exit code:' "$exit_ec"; }
 trap trap_exit EXIT
 
+gh auth status
+echo exit, gh auth status, $?
+
 get_gh_auth_stat=$( gh auth status )
 
 count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -l )
