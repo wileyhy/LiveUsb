e94748d4b62b75b3d7fc54138b82b73c6270156b
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 12:21:29 -0700

    LiveUsb1.sh, more silencing of firefox's terminal output
    Add a "1>&2" on line 1324
    Add a `sleep5 5` after `firefox` and before gh_auth_login_command()

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -1321,7 +1321,8 @@ if ! gh auth status 2>/dev/null 1>&2 || [[ $( gh auth status |& grep --count $'\
 then
   if ! pgrep 'firefox'
   then
-    firefox --browser 2>/dev/null &
+    firefox --browser 2>/dev/null 1>&2 &
+    sleep 5
     pause_to_check "$nL" 'Waiting till browser is open before running  gh auth  command'
     gh_auth_login_command
   fi
