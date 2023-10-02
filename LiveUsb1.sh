bd9a23ff010272e7ae045c9c039fec340535d31a
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 14:29:14 -0700

    LiveUsb1.sh, debugging
    I don't know why when the assignment to $get_gh_auth_stat was uncommented and the ERR trap was disabled the pipefail error at the assignment to $count_gh_auth_credentials failed to rematerialize. I suspect it's my fault, though, and I really don't want to go any further into this set of bugs, since now I can't `git pusush` on the command line either. So I'm just reverting to the functional `printf -v` command from, like, 30 commits ago, and here'e hoping.

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -1346,8 +1346,8 @@ unset KK gh_config_list_out github_configs
 :;: 'GH -- Login to github'
 ## Note, executing this command just once
 #get_gh_auth_stat=$( gh auth status )
-count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -l )
-#printf -v count_gh_auth_checkmarks '%s' $( gh auth status |& grep --count $'\xe2\x9c\x93' )
+#count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -l )
+printf -v count_gh_auth_checkmarks '%s' $( gh auth status |& grep --count $'\xe2\x9c\x93' )
 
   wait -f # <>
   declare -p count_gh_auth_checkmarks
