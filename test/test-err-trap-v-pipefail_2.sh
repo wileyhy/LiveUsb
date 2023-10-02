f065c2a757d47c42fbf452df0ccce70d9c89c69c
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 15:00:33 -0700

    ./test/test-err-trap-v-pipefail_2.sh, debugging
    Remove xtrace output that was relevant for version 1 of this test script

--- test/test-err-trap-v-pipefail_2.sh
+++ test/test-err-trap-v-pipefail_2.sh
@@ -14,24 +14,3 @@ count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -
 builtin exit $LINENO
 
 ###
-
-[liveuser@localhost-live LiveUsb]$ ./test/test-err-trap-v-pipefail.sh
-+ gh auth logout
-not logged in to any hosts
-+ set -o pipefail
-+ trap trap_err ERR
-++ gh auth status
-You are not logged into any GitHub hosts. Run gh auth login to authenticate.
-+ get_gh_auth_stat=
-++ trap_err
-++ local - ec=1
-++ : 'exit code:' 1
-++ gh auth status
-++ grep --only ✓
-++ wc -l
-+ count_gh_auth_checkmarks=0
-++ trap_err
-++ local - ec=1
-++ : 'exit code:' 1
-+ builtin exit 14
-
