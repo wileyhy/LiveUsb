38a66120b2d0cbf45dae767cf25b95f9692bf3cb
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 15:03:21 -0700

    ./test/test-err-trap-v-pipefail_2.sh, debugging
    Add an EXIT trap and a function definition of trap_exit()
    Add an `echo foo` at line 17, before `builtin exit`

--- test/test-err-trap-v-pipefail_2.sh
+++ test/test-err-trap-v-pipefail_2.sh
@@ -4,12 +4,17 @@ gh auth logout
 
 set -o pipefail
 
-#trap_err(){ local - ec="$?"; : 'exit code:' "$ec"; }
+#trap_err(){ local - err_ec="$?"; : 'exit code:' "$err_ec"; }
 #trap trap_err ERR
+
+trap_exit(){ local - exit_ec="$?"; : 'exit code:' "$exit_ec"; }
+trap trap_exit EXIT
 
 get_gh_auth_stat=$( gh auth status )
 
 count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -l )
+
+echo foo $LINENO
 
 builtin exit $LINENO
 
