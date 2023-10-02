f6825fe13137dcbdeab69ad93b2d6156c6aa2ca8
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 14:17:13 -0700

    LiveUsb1.sh, debugging
    At the assignment of $get_gh_auth_stat, `gh auth status` returning exit code 1 activates the ERR trap, but does not activate pipefail.
    Comment `trap` on ERR, lines 1239 and 1240

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -1236,8 +1236,8 @@ function write_ssh_conf() { :
 
 
 
-:;: 'Define trap on ERR'
-trap trap_err ERR
+#:;: 'Define trap on ERR'
+#trap trap_err ERR
 
 :;: 'Define trap on EXIT'
 trap trap_exit EXIT
