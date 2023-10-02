96c00bb5f77ea878b9300a9edcd89031f186355f
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 15:10:42 -0700

    ./test/test-err-trap-v-pipefail_2.sh, debugging
    Reviewing commit e0adb97 on github.com and rewriting this test script to mirror those conditions

--- test/test-err-trap-v-pipefail_2.sh
+++ test/test-err-trap-v-pipefail_2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -x
+set -o pipefail
 
 gh auth logout
 
-set -o pipefail
-
-#trap_err(){ local - err_ec="$?"; : 'exit code:' "$err_ec"; }
-#trap trap_err ERR
+trap_err(){ local - err_ec="$?"; : 'exit code:' "$err_ec"; }
+trap trap_err ERR
 
 trap_exit(){ local - exit_ec="$?"; : 'exit code:' "$exit_ec"; }
 trap trap_exit EXIT
 
-gh auth status
-echo exit, gh auth status, $?
+#gh auth status
+#echo exit, gh auth status, $?
 
-get_gh_auth_stat=$( gh auth status )
+#get_gh_auth_stat=$( gh auth status )
 
 count_gh_auth_checkmarks=$( gh auth status |& grep --only $'\xe2\x9c\x93' | wc -l )
 
