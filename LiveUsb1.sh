1bc7d7c18ef4b0a6d55dc19336a0c351e5d51f6c
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-02 17:02:59 -0700

    LiveUsb1.sh, add comment

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -652,8 +652,9 @@ function setup_git(){ :
   read -r -a prev_umask < <( umask -p )
   umask 133
 
-  local regexp="(/etc|~)/[.]*git"
+  local regexp="(/etc|~)/[.]*git"           ## Note, I doubt the accuracy of this RE
   local ZZ
+
   for ZZ in "${!git_files_a[@]}"
   do
       #declare -p git_files_a ZZ
