8d62eb2eacc95083a658c8faac3cb9ac90a73f14
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-08 21:25:31 -0700

    LiveUsb1.sh, comment an if-fi block
    The functionality of the code had been duplicated elsewhere

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -559,11 +559,12 @@ function reqd_user_files(){ :
         if ! [[ -e "${source_file}" ]]
         then
 
-          : 'If the USB device is NA, then exit'
-          if [[ -z $pttn_path ]]
-          then
-            die "USB device not available, ${pttn_label}"
-          fi
+          ## Duplicated code
+          #: 'If the USB device is NA, then exit'
+          #if [[ -z $pttn_path ]]
+          #then
+            #die "USB device not available, ${pttn_label}"
+          #fi
 
           : 'If the partition is not mounted which holds the data directory, then mount it'
           if ! grep --quiet "${mount_pt}" <<< "${lsblk_out[@]}" # <>
