80068416397a12bc08cc45201e7a45a2e3caf2d4
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-22 05:43:52 -0700

    LiveUsb1.sh, bugfix, change x4 grave accents for single quotes
    Line 391

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -388,7 +388,7 @@ function increase_disk_space(){ :
           declare ls_out
           readarray -t ls_out < <( ls -l --all --human-readable --classify --inode --directory --zero "${JJ}" )
           ## Note, `\x60` is a "grave accent"
-          printf `%s, output of %bls%b, %s \n` "${scr_nm}" `\x60` `\x60` "$( realpath -e "${JJ}" )"
+          printf `%s, output of %bls%b, %s \n` "${scr_nm}" '\x60' '\x60' "$( realpath -e "${JJ}" )"
           printf `%s\n` "${ls_out[@]}"
           unset ls_out
 
