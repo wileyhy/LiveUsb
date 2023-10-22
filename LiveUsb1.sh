1a7a13ad5c9e0e6d351a0eca75246218a11746dd
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-10-22 12:24:21 -0700

    LiveUsb1.sh, add debug note

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -74,7 +74,7 @@ alias .^:=': $color_reset ; :'
   readonly fn_bndry_sh fn_bndry_lo
   fn_lvl=0
 
-  alias function_boundary_in='local - loc_hyphn="$-" loc_exit_code="$?" loc_lineno="${LN:-$nL}" loc_undersc="$_" _="${fn_bndry_sh} ${FUNCNAME[0]}() BEGINS ${fn_bndry_sh} ${fn_lvl} to $(( ++fn_lvl ))"'
+  alias function_boundary_in='local - loc_hyphn="$-" loc_exit_code="$?" loc_lineno="${LN:-$nL}" loc_undersc="$_" _="${fn_bndry_sh} ${FUNCNAME[0]}() BEGINS ${fn_bndry_sh} ${fn_lvl} to $(( ++ fn_lvl ))"' ## AJAX -- see "++ " >> "++"
   alias function_boundary_out='true "${fn_bndry_lo} ${FUNCNAME[1]}()  ENDS  ${fn_bndry_sh} ${fn_lvl} to $(( --fn_lvl ))"'
 
   user_real_name="Wiley Young"
