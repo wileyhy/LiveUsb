b1dc136876674e6ea4a1f70b265751518914cfd4
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-11-06 22:08:49 -0800

    LiveUsb1.sh, more style edits re xtrace

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -673,7 +673,7 @@ function reqd_user_files(){ als_function_boundary_in
     do
       :;: 'Loop A:1 - open \\\ '
 
-      :;: "If the target conf file/dir does not exist"
+      : "If the target conf file/dir does not exist"
       if ! [[ -e ${QQ[BB]} ]]
       then
 
@@ -716,7 +716,7 @@ function reqd_user_files(){ als_function_boundary_in
         rsync_install_if_missing  "${source_file}" "${dest_dir}"
         unset source_file dest_dir
       fi
-      :;: "Loop A:1 - shut /// " ;:;:
+      : "Loop A:1 - shut /// " ;:;:
     done
     :;: "Loops A:1 - complete === " ;:
 
