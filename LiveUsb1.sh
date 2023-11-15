72f1bd6cc3896c7f933fb75e299486880a6fee39
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-11-14 20:50:02 -0800

    LiveUsb1.sh, line251, remove a relative directory ref
    replace relative directory link with an absolute path in variable $dev_d1

--- LiveUsb1.sh
+++ LiveUsb1.sh
@@ -248,7 +248,7 @@ function clone_repo(){ als_function_boundary_in
   [[ ${PWD} = "${dev_d1}" ]] || die
 
   local hash_of_read_me_file
-  hash_of_read_me_file=$( sha256sum "./${scr_repo_nm}/README.md" | cut -d" " -f1 )
+  hash_of_read_me_file=$( sha256sum "${dev_d1}/${scr_repo_nm}/README.md" | cut -d" " -f1 )
 
   if ! [[ -d ./${scr_repo_nm} ]] || ! [[ -f ./${scr_repo_nm}/README.md ]] ||
       ! [[ ${hash_of_read_me_file} = "${sha256_of_repo_readme}" ]]
