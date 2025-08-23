522810d851d0b0c0185527e4785aaf1b885404bf
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2025-08-23 14:54:54 -0700

    add whitespace

--- free_disk_space.sh
+++ free_disk_space.sh
@@ -32,10 +32,9 @@
 
 
 
-
-
 #####################################################################
 : "${C1} Functions ${C0}"
+
 
 : "${C1}Define function write_list_a\ctual${C0}"
 write_Lst_actual() {
@@ -44,22 +43,26 @@ write_Lst_actual() {
   declare -p list_actual > "${ff_ListActual}"
 }
 
+
 : "${C1}Define function define_count_s\aved_state${C0}"
 define_count_saved_state() {
 	: "${C1}...and the integer in in the variable #count_s\aved_state# should be defined${C0}"
 	count_saved_state="${#array_saved_state_pkgnms[@]}"
 }
 
+
 : "${C1}Define function copy_list_as_s\aved_state${C0}"
 copy_list_as_saved_state() {
 	array_saved_state_pkgnms=( "${list_actual[@]}" )
 }
+
 
 : "${C1}Define function read_in_array_s\aved_state_pkgnms${C0}"
 read_in_Array_saved_state_pkgNms() {
 	{ mapfile -t array_saved_state_pkgnms < "${ff_ListSavedState}" && [[ -n ${array_saved_state_pkgnms[*]:0:1} ]]; } || 
 		exit "${LINENO}"
 }
+
 
 : "${C1}Define function write_array_s\aved_state_pkgnms${C0}"
 write_array_Saved_state_pkgNms() {
