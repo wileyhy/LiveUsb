0dcea9abed47d7db53f8f8431cfdae60df50c9a5
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2025-08-23 20:23:58 -0700

    debugging

--- get_file_atimes.sh
+++ get_file_atimes.sh
@@ -72,7 +72,7 @@ do
   
     echo "count, some_files: ${#some_files[@]}" #<>
 
-  if [[ -e "${some_files[0]:0:8}" ]]
+  if [[ -e "${some_files[0]}" ]]
   then
     sudo -- stat --printf='%W %N\n' "${some_files[@]}" \
       | sudo tee -a "${II}" >/dev/null \
