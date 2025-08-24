b1b6e0919051f0d820ea07afaf6d6c3060b3d486
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2025-08-24 11:39:00 -0700

    edit debug code

--- get_file_atimes.sh
+++ get_file_atimes.sh
@@ -82,7 +82,7 @@ full_count_allFiles=${#all_files[@]}
 
   echo "all_files[0]: ${all_files[0]}" #<>
   echo "full_count_allFiles: ${full_count_allFiles}" #<>
-  exit "${LINENO}" #<>
+  #exit "${LINENO}" #<>
 
 
 ## Get all the atimes
