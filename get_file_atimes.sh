ba8e8af7945bed8d6aace2d7a8448d6dcedf99df
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2025-08-26 13:23:51 -0700

    edit debug code

--- get_file_atimes.sh
+++ get_file_atimes.sh
@@ -3,7 +3,7 @@
 
 shopt -s extglob
 
-  #set -x #<>
+  set -x #<>
   set -euo pipefail #<>
 
 
@@ -66,6 +66,9 @@ if [[ -f ${EE} ]] \
 then
   sudo mv "${EE}" "${NN}"
 fi
+
+  exit "${LINENO}" #<>
+  set -x #<>
 
 # Clean the filesystem - remove broken symlinks
 time sudo bash -O globstar -c \
