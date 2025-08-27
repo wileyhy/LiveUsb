3a530880eec404bf4531c55a7376b361b58022a4
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2025-08-27 15:50:37 -0700

    add functions

--- find-weird-filenames.sh
+++ find-weird-filenames.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# find-weird-filenames.s
+# find-weird-filenames.sh
 #
-# At a certain point, it's easier to clearlist all filenames be 
-# "Names" according to Bash syntax rules. 
+# At a certain point, it\s easier to clearlist all filenames be 
+# \Names\ according to Bash syntax rules. 
 
   set -x
   set -euo pipefail
@@ -11,24 +11,108 @@
 
 
 
+sudo -v 
+
+
+# Usage: _Fn_find_chars_ "${input}"
+#
+function _Fn_find_chars_ (){
+  local input
+  input=$1
+  mapfile -d "" -t files < <(
+    sudo find / -name '*'"${input}"'*' -print0
+  )
+}
+
+# Usage: _Fn_find_words_ "${input}"
+#
+function _Fn_find_words_ (){
+  local input
+  input=$1
+  mapfile -d "" -t files < <(
+    sudo find / -name '*'"${input}"'*' -print0 2>&1 \
+      | grep -Eze '\<'"${input}"'\>'
+  )
+}
+
+
+# Usage: _Fn_get_files_ $'\n'
+#
+function _Fn_get_files_ (){
+  local - \
+    && set -x
+  
+  if [[ $# -eq 2 ]]
+  then
+    local lin
+    lin=$1
+    shift
+  elif [[ $# -ne 1 ]]
+  then
+    printf 'Error, line %d: fn reqs x1 non-lineno argument.\n'
+    exit "${lin:-${LINENO}}"
+  fi
+
+  local input
+  input=$1
+  local -a files
+  file=( )
+
+  if [[ ${input} =~ ([A-Za-z]+) ]]
+  then
+    _Fn_find_words_ "${input}"
+  else
+    _Fn_find_chars_ "${input}"
+  fi
+
+  printf '%d' "${#files[@]}"
+  return 00
+}
+
+
 # Characters illegal for filenames in Linux
 # /
-# $'\0'
+_Fn_get_files_ '/'
+
+# <NUL>
+_Fn_get_files_ $'\0'
 
 # Execution contexts
 # exec
-# eval
-# $((
-# $(<
-# ${|
-# ${c
-# ((
-# ))
-# $(
-# ${
-# `
+_Fn_get_files_ "exec"
 
-# Bash 5.2 Control operators (which are not also single metacharacters)
+# eval
+_Fn_get_files_ "eval"
+
+# $((
+_Fn_get_files_
+
+# $(<
+_Fn_get_files_
+
+# ${|
+_Fn_get_files_
+
+# ${c
+_Fn_get_files_
+
+# ((
+_Fn_get_files_
+
+# ))
+_Fn_get_files_
+
+# $(
+_Fn_get_files_
+
+# ${
+_Fn_get_files_
+
+# `
+_Fn_get_files_
+
+
+# Bash 5.2 Control operators (which are not also single metac\haracters)
 # ;;&
 # ;;
 # ||
@@ -36,7 +120,7 @@
 # ;&
 # |&
 
-# Bash 5.2 Redirection operators (which are not also single metacharacters)
+# Bash 5.2 Redirection operators (which are not also single metac\haracters)
 # <&digit- 
 # >&digit- 
 # <&- 
@@ -52,7 +136,7 @@
 # >& 
 # &> 
 
-# POSIX-2024 Shell metacharacters
+# POSIX-2024 Shell metac\haracters
 
 # <newline> $'\n'
 # <tab> $'\t'
@@ -68,14 +152,14 @@
 # POSIX-2024 Shell line continuation
 # \$'\n'
 
-# POSIX-2024 Quoting characters
+# POSIX-2024 Quoting c\haracters
 # $'
 # $"
 # /
 # '
 # "
 
-# Shell comment characters
+# Shell comment c\haracters
 # '#'
 # ': ' (rare)
 
@@ -103,7 +187,7 @@
 # }
 # !
 
-# Pathname expansion characters (globs)
+# Pathname expansion c\haracters (globs)
 # *
 # ?
 # [:
@@ -114,7 +198,7 @@
 # Bash 5.2 Extglobs
 # 
 
-# Tilde expansion characters (globs)
+# Tilde expansion c\haracters (globs)
 # ~+digit
 # ~-digit
 # ~digit
@@ -122,12 +206,12 @@
 # ~-
 # ~
 
-# Brace Expansion characters
+# Brace Expansion c\haracters
 # {,
 # ,}
 # ,
 
-# Parameter Expansion characters
+# Parameter Expansion c\haracters
 # ${
 
 # Bash 5.2 Special Parameters
